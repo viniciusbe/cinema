8b0a8d645492f158bffc4b0cbac91b2a9dfb5c82
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-09-12 23:54:04 -0300

    feat: cli scratch

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"bufio"
-	"cinema/entity"
-	gormdb "cinema/infra/gormdb"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func StringPrompt(label string) string {
+func StringPrompt() string {
 	var s string
 	r := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Fprint(os.Stderr, label+" ")
 		s, _ = r.ReadString('\n')
 		if s != "" {
 			break
@@ -23,24 +20,68 @@ func StringPrompt(label string) string {
 }
 
 func main() {
-	descricaoGenero := StringPrompt("Qual é seu nome")
+	input := ""
 
-	db, err := gormdb.SetupGormDB()
+	for {
+		fmt.Println("Cinema - Menu principal:")
 
-	if err != nil {
-		panic(err)
+		fmt.Println("[1] Generos")
+		fmt.Println("[1] Filmes")
+		fmt.Println("[1] Diretores")
+		fmt.Println("[1] Ingressos")
+
+		input = StringPrompt()
+
+		if input == "s" {
+			fmt.Println("Programa finalizado")
+			break
+		}
+
+		for {
+			fmt.Println("[1] Listar todos")
+			fmt.Println("[2] Criar")
+			fmt.Println("[3] Editar")
+			fmt.Println("[4] Excluir")
+			fmt.Println("[5] Voltar")
+
+			input = StringPrompt()
+
+			if input == "5" {
+				break
+			}
+
+			switch input {
+			case "1":
+				fmt.Println("Listar")
+			case "2":
+				fmt.Println("Criar")
+			case "3":
+				fmt.Println("Editar")
+			case "4":
+				fmt.Println("Excluir")
+			}
+
+		}
 	}
 
-	db.AutoMigrate(&entity.Gender{})
+	// descricaoGenero := StringPrompt()
 
-	newGender := entity.Gender{Descricao: descricaoGenero}
+	// db, err := gormdb.SetupGormDB()
 
-	db.Create(&newGender)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	var matchingGender entity.Gender
-	// var genders []entity.Gender
-	db.First(&matchingGender, newGender.ID)
+	// db.AutoMigrate(&entity.Gender{})
 
-	fmt.Println(matchingGender.Descricao)
+	// newGender := entity.Gender{Descricao: descricaoGenero}
+
+	// db.Create(&newGender)
+
+	// var matchingGender entity.Gender
+	// // var genders []entity.Gender
+	// db.First(&matchingGender, newGender.ID)
+
+	// fmt.Println(matchingGender.Descricao)
 
 }
