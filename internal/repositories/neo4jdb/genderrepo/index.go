36e1bcad2f995b06dfc55251d3f1711c52c0351d
Vinícius Bernardes <vinicius@bernardes.dev>
2023-12-03 10:45:09 -0300

    feat (Gender): create update, delete and getById repository methods

--- internal/repositories/neo4jdb/genderrepo/index.go
+++ internal/repositories/neo4jdb/genderrepo/index.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j/db"
 )
 
 type Repository struct {
@@ -22,7 +23,6 @@ func NewNeo4jRepository(driver neo4j.DriverWithContext, ctx context.Context) por
 }
 
 func (r *Repository) ListAll() ([]entities.Gender, error) {
-	var genders []entities.Gender
 	ctx := r.ctx
 	session := r.driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
 	defer session.Close(ctx)
@@ -44,28 +44,42 @@ func (r *Repository) ListAll() ([]entities.Gender, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Erro ao listar gêneros -> %w", err)
 	}
-	for _, person := range gendersResult.([]*neo4j.Record) {
 
-		var gender entities.Gender
-		// gender.Description,_ = gendersResult
-		rawPersonNode, _ := person.Get("g")
-
-		// gender.Description, _, _ = neo4j.GetRecordValue[string](person, "gender")
-		gender.Description, _ = neo4j.GetProperty[string](rawPersonNode.(neo4j.Node), "description")
-		gender.GenderID, _ = person.AsMap()["name"].(string)
-
-		fmt.Println(gender.Description)
+	var genders []entities.Gender
+	for _, record := range gendersResult.([]*neo4j.Record) {
+		gender := GenderFromRecord(record)
+		genders = append(genders, *gender)
 	}
 
 	return genders, nil
 }
 
 func (r *Repository) Find(id string) (*entities.Gender, error) {
-	var gender *entities.Gender
+	ctx := r.ctx
+	session := r.driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
+	defer session.Close(ctx)
 
-	// if err != nil {
-	// 	return nil, fmt.Errorf("Erro ao encontrar gênero -> %w", err)
-	// }
+	genderRecord, err := session.ExecuteRead(ctx,
+		func(tx neo4j.ManagedTransaction) (any, error) {
+			result, err := tx.Run(ctx, `MATCH (g:Gender {genderID: $id}) RETURN g`, map[string]any{
+				"id": id,
+			})
+			if err != nil {
+				return nil, err
+			}
+			record, err := result.Single(ctx)
+			if err != nil {
+				return nil, err
+			}
+
+			return record, nil
+		})
+
+	if err != nil {
+		return nil, fmt.Errorf("Erro ao encontrar gênero -> %w", err)
+	}
+
+	gender := GenderFromRecord(genderRecord.(*db.Record))
 
 	return gender, nil
 }
@@ -80,19 +94,59 @@ func (r *Repository) Insert(gender *entities.Gender) error {
 }
 
 func (r *Repository) Save(gender *entities.Gender) error {
+	ctx := r.ctx
+	session := r.driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
+	defer session.Close(ctx)
 
-	// if err != nil {
-	// 	return fmt.Errorf("Erro ao atualizar gênero -> %w", err)
-	// }
+	_, err := session.ExecuteWrite(ctx,
+		func(tx neo4j.ManagedTransaction) (any, error) {
+			result, err := tx.Run(ctx, `MATCH (g:Gender {genderID: $id}) SET g.description = $description`, map[string]any{
+				"id":          gender.GenderID,
+				"description": gender.Description,
+			})
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
+		})
+
+	if err != nil {
+		return fmt.Errorf("Erro ao atualizar gênero -> %w", err)
+	}
 
 	return nil
 }
 
 func (r *Repository) Delete(id string) error {
+	ctx := r.ctx
+	session := r.driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
+	defer session.Close(ctx)
 
-	// if err != nil {
-	// 	return fmt.Errorf("Erro ao excluir gênero -> %w", err)
-	// }
+	_, err := session.ExecuteWrite(ctx,
+		func(tx neo4j.ManagedTransaction) (any, error) {
+			result, err := tx.Run(ctx, `MATCH (g:Gender {genderID: $id}) DELETE g`, map[string]any{
+				"id": id,
+			})
+			if err != nil {
+				return nil, err
+			}
+			return result, nil
+		})
+
+	if err != nil {
+		return fmt.Errorf("Erro ao excluir gênero -> %w", err)
+	}
 
 	return nil
+}
+
+func GenderFromRecord(record *db.Record) *entities.Gender {
+	rawGenderNode, _ := record.Get("g")
+	genderNode := rawGenderNode.(neo4j.Node)
+
+	var gender entities.Gender
+	gender.Description, _ = neo4j.GetProperty[string](genderNode, "description")
+	gender.GenderID, _ = neo4j.GetProperty[string](genderNode, "genderID")
+
+	return &gender
 }
